bc34b0f79ba903b482ff04353b8fc0cbcc545424
manji 3 <111729787+manjisama1@users.noreply.github.com>
2026-08-02 20:21:01 +0530

    Updated plugin data 2aug, 20:21

--- src/data/permanentPlugins.js
+++ src/data/permanentPlugins.js
@@ -1,97 +1,114 @@
 export const PERMANENT_PLUGINS = [
-  {
-    "id": "plugin-VYAYpC",
-    "name": "slot",
-    "author": "manjisama1",
-    "description": "slot game",
-    "type": "game",
-    "gistLink": "https://gist.github.com/manjisama1/a408014c978a4a599ed601da0de53aa0",
-    "tags": [
-      "slot"
-    ],
-    "features": [
-      "game"
-    ]
-  },
-  {
-    "id": "plugin-GshBCL",
-    "name": "readmore",
-    "author": "manjisama1",
-    "description": "Make a readmore text message",
-    "type": "fun",
-    "gistLink": "https://gist.github.com/manjisama1/091b5798c7144e53c5a446cb2d04e819",
-    "tags": [
-      "read more"
-    ],
-    "features": [
-      "text",
-      "read more"
-    ]
-  },
-  {
-    "id": "plugin-vwLLyX",
-    "name": "page",
-    "author": "manjisama1",
-    "description": "Make a page of any dimension and colour",
-    "type": "tool",
-    "gistLink": "https://gist.github.com/manjisama1/f0ce4f88faf953e62dcb39e567e1b2cd",
-    "tags": [
-      "edit",
-      "page"
-    ],
-    "features": [
-      "edit"
-    ]
-  },
-  {
-    "id": "plugin-1768883897387",
-    "name": "T&D",
-    "author": "manjisama1",
-    "description": "Truth and dare, would you rather, never have I ever.",
-    "type": "game",
-    "gistLink": "https://gist.github.com/manjisama1/f7bcdf7b508d240b978ccb2b713d1c28",
-    "tags": [
-      "game",
-      "fun",
-      "t&d",
-      "truth or dare"
-    ],
-    "features": [
-      "truth or dare"
-    ],
-    "likes": 0,
-    "status": "approved",
-    "createdAt": "2026-01-20T04:38:17.387Z"
-  },
-  {
-    "id": "plugin-0N9kqA",
-    "name": "interactions",
-    "author": "manjisama1",
-    "description": "angry, baka, bite, blush, bored, cry, cuddle, dance, facepalm, feed, handhold, handshake, happy, highfive, hug, kick, kiss, laugh, lurk, nod, nom, nope, pat, peck, poke, pout, punch, run, shoot, shrug, slap, sleep, smile, smug, stare, think, thumbsup, tickle, wave, wink, yawn, yeet",
-    "type": "fun",
-    "gistLink": "https://gist.github.com/manjisama1/a8059a734d72d88d4aac81137ff65033",
-    "tags": [
-      "react",
-      "interaction"
-    ],
-    "features": [
-      "interact"
-    ]
-  },
-  {
-    "id": "plugin-xPrNVA",
-    "name": "dld",
-    "author": "manjisama1",
-    "description": "download any youtube video, trim yt videos, or download as audio",
-    "type": "download",
-    "gistLink": "https://gist.github.com/manjisama1/dd26f21dd56da3cd99732816067fb863",
-    "tags": [
-      "download",
-      "media"
-    ],
-    "features": [
-      "youtube"
-    ]
+{
+  "id": "plugin-VYAYpC",
+  "name": "slot",
+  "author": "manjisama1",
+  "description": "slot game",
+  "type": "game",
+  "gistLink": "https://gist.github.com/manjisama1/a408014c978a4a599ed601da0de53aa0",
+  "tags": [
+    "slot"
+  ],
+  "features": [
+    "game"
+  ]
+},
+{
+  "id": "plugin-GshBCL",
+  "name": "readmore",
+  "author": "manjisama1",
+  "description": "Make a readmore text message",
+  "type": "fun",
+  "gistLink": "https://gist.github.com/manjisama1/091b5798c7144e53c5a446cb2d04e819",
+  "tags": [
+    "read more"
+  ],
+  "features": [
+    "text",
+    "read more"
+  ]
+},
+{
+  "id": "plugin-vwLLyX",
+  "name": "page",
+  "author": "manjisama1",
+  "description": "Make a page of any dimension and colour",
+  "type": "tool",
+  "gistLink": "https://gist.github.com/manjisama1/f0ce4f88faf953e62dcb39e567e1b2cd",
+  "tags": [
+    "edit",
+    "page"
+  ],
+  "features": [
+    "edit"
+  ]
+},
+{
+  "id": "plugin-1768883897387",
+  "name": "T&D",
+  "author": "manjisama1",
+  "description": "Truth and dare, would you rather, never have I ever.",
+  "type": "game",
+  "gistLink": "https://gist.github.com/manjisama1/f7bcdf7b508d240b978ccb2b713d1c28",
+  "tags": [
+    "game",
+    "fun",
+    "t&d",
+    "truth or dare"
+  ],
+  "features": [
+    "truth or dare"
+  ],
+  "likes": 0,
+  "status": "approved",
+  "createdAt": "2026-01-20T04:38:17.387Z"
+},
+{
+  "id": "plugin-0N9kqA",
+  "name": "interactions",
+  "author": "manjisama1",
+  "description": "angry, baka, bite, blush, bored, cry, cuddle, dance, facepalm, feed, handhold, handshake, happy, highfive, hug, kick, kiss, laugh, lurk, nod, nom, nope, pat, peck, poke, pout, punch, run, shoot, shrug, slap, sleep, smile, smug, stare, think, thumbsup, tickle, wave, wink, yawn, yeet",
+  "type": "fun",
+  "gistLink": "https://gist.github.com/manjisama1/a8059a734d72d88d4aac81137ff65033",
+  "tags": [
+    "react",
+    "interaction"
+  ],
+  "features": [
+    "interact"
+  ]
+},
+{
+  "id": "plugin-xPrNVA",
+  "name": "dld",
+  "author": "manjisama1",
+  "description": "download any youtube video, trim yt videos, or download as audio",
+  "type": "download",
+  "gistLink": "https://gist.github.com/manjisama1/dd26f21dd56da3cd99732816067fb863",
+  "tags": [
+    "download",
+    "media"
+  ],
+  "features": [
+    "youtube"
+  ]
+},
+{
+  "id": "plugin-dj4Z2P",
+  "name": "String",
+  "author": "manjisama1",
+  "description": "Manipulate and convert strings",
+  "type": "tool",
+  "gistLink": "https://gist.github.com/manjisama1/c6a421b7917716cb494a067cd8eb6957",
+  "tags": [
+    "Tools",
+    "UTF-8"
+  ],
+  "features": [
+    "Binary",
+    "Morse",
+    "base64 to string"
+  ]
   }
 ];
 
